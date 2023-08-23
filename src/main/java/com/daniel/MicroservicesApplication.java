6aec835059e89d5dc64f1b4003d5fd7a3249a47b
daniel <jabodav.daniel@gmail.com>
2023-08-23 23:33:15 +0300

    first commit

---
+++ src/main/java/com/daniel/MicroservicesApplication.java
@@ -0,0 +1,15 @@
+package com.daniel.microservicesapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(MicroservicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/daniel/MicroservicesApplicationTests.java
@@ -0,0 +1,15 @@
+package com.daniel.microservicesapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
